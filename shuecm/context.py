507e6e88a08bd6134c11b1c4e934033a7c73d7a4
prostomarkeloff <botyavs@gmail.com>
2019-10-06 01:50:53 +0300

    delete unnecessary code

--- shuecm/context.py
+++ shuecm/context.py
@@ -3,27 +3,11 @@ Contextvars for shuecm life cycle.
 """
 from contextvars import ContextVar
 
-from pydantic import BaseModel
-from vk import VK
-
 from db.models.chat import Chat
 from db.models.user import User
 from db.models.user import UserInChat
 
-
-class Bot(BaseModel):
-    group_id: int
-    access_token: str
-
-
+# it`s will be set in middlewares, may be in args validators.
 current_user: ContextVar[User] = ContextVar("current_user")
 current_chat: ContextVar[Chat] = ContextVar("current_chat")
 current_user_in_chat: ContextVar[UserInChat] = ContextVar("current_user_in_chat")
-current_bot: ContextVar[Bot] = ContextVar("current_bot")  # for supporting user groups
-
-
-# now this not used.
-def change_bot_context(bot: Bot) -> None:
-    vk = VK.get_current()
-    current_bot.set(bot)
-    vk.access_token = bot.access_token
