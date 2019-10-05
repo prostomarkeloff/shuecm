3fbae7135150500f6baf3a35ff1b3cccd101c6a3
prostomarkeloff <botyavs@gmail.com>
2019-10-05 19:11:49 +0300

    fix configs ;)

--- db/config.py
+++ db/config.py
@@ -1,7 +1,7 @@
 from environs import Env
 
 env_db: Env = Env()
-env_db.read_env("..database.env.example", recurse=False)
+env_db.read_env(".database.env", recurse=False)
 
 MONGODB_CONNECTION_URI: str = env_db("MONGODB_CONNECTION_URI")
 MONGODB_DATABASE_NAME: str = env_db("MONGODB_DATABASE_NAME")
--- shuecm/config.py
+++ shuecm/config.py
@@ -1,14 +1,14 @@
 from environs import Env
 
 env_bot: Env = Env()
-env_bot.read_env("..bot.env.example", recurse=False)
+env_bot.read_env(".bot.env", recurse=False)
 
 VK_TOKEN: str = env_bot("VK_TOKEN")
 VK_GROUP_ID: int = env_bot.int("VK_GROUP_ID")
 LOGGING_LEVEL: str = env_bot("LOGGING_LEVEL")
 
 env_general: Env = Env()
-env_general.read_env(".general.env.example", recurse=False)
+env_general.read_env(".general.env", recurse=False)
 
 SENTRY_DSN: str = env_general("SENTRY_DSN")
 PRODUCTION: bool = env_general.bool("PRODUCTION")
