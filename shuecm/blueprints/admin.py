1e8bff241ff1e710247bd07a349d88967a165c01
prostomarkeloff <botyavs@gmail.com>
2019-10-13 20:29:09 +0300

    fix kick

--- shuecm/blueprints/admin.py
+++ shuecm/blueprints/admin.py
@@ -4,11 +4,14 @@ Blueprint for informational commands.
 from vk import types
 from vk import VK
 from vk.bot_framework.dispatcher import Blueprint
+from vk.exceptions.errors import APIException
+from vk.types.events.community.event import MessageNew
 
 from db.structs.status import Permission
 from shuecm.context import current_chat
 from shuecm.utils import check_role_priority
 from shuecm.utils import format_chat_id
+
 
 bp = Blueprint()
 api = VK.get_current().get_api()
@@ -18,15 +21,22 @@ api = VK.get_current().get_api()
     texts=["кик"], with_permissions=[Permission.CAN_KICK], with_reply_message=True
 )
 async def handle_kick(message: types.Message, data: dict):
+    if message.reply_message.from_id == -MessageNew.get_current().group_id:
+        return await message.answer(
+            "⛔ Ваша роль в беседе ниже чем роль того, кого Вы пытаетесь исключить."
+        )
     can_this = await check_role_priority(message.reply_message.from_id)
     if not can_this:
         return await message.answer(
             "⛔ Ваша роль в беседе ниже чем роль того, кого Вы пытаетесь исключить."
         )
-    await api.messages.remove_chat_user(
-        chat_id=format_chat_id(current_chat.get().chat_id),
-        member_id=message.reply_message.from_id,
-    )
+    try:
+        await api.messages.remove_chat_user(
+            chat_id=format_chat_id(current_chat.get().chat_id),
+            member_id=message.reply_message.from_id,
+        )
+    except APIException:
+        return await message.answer("⛔ Упс.. Что-то пошло не так.")
     await message.answer("✅ Пользователь успешно удалён!")
 
 
